e9287784a44cf9e17510564dfc4bbe5c879882c0
Ahamed Aathil <ahamedaathil.5@gmail.com>
2022-09-23 20:24:40 +0530

    Home page created

--- app/src/main/java/com/example/sliitguru/home.java
+++ app/src/main/java/com/example/sliitguru/home.java
@@ -2,13 +2,53 @@ package com.example.sliitguru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home extends AppCompatActivity {
+    Button gpa,todo,reminder,linker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        gpa=findViewById(R.id.gpa);
+        todo=findViewById(R.id.todo);
+        reminder=findViewById(R.id.reminder);
+        linker=findViewById(R.id.social);
+
+        gpa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(home.this,gpa_calc.class);
+                startActivity(i);
+            }
+        });
+
+        todo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent j = new Intent(home.this,register.class);
+                startActivity(j);
+            }
+        });
+
+        reminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent k = new Intent(home.this,register.class);
+                startActivity(k);
+            }
+        });
+
+        linker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent l = new Intent(home.this,register.class);
+                startActivity(l);
+            }
+        });
     }
 }
